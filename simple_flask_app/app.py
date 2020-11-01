e46b181621a9c79e29ff229be75d174b806aa438
Madkerberos <giuseppe.maddiona@gmail.com>
2020-11-01 18:07:03 +0100

    update app code

--- simple_flask_app/app.py
+++ simple_flask_app/app.py
@@ -2,10 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello APP'
-
 @app.route('/api')
 def hello_world():
     return 'Hello, World Giuseppe 2!'
